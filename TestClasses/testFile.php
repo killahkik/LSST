93f0bf0a0560636d60c3804916287558bf72dde7
FoolyxCooly <82619286+FoolyxCooly@users.noreply.github.com>
2024-11-22 20:50:13 -0500

    test file update

--- TestClasses/testFile.php
+++ TestClasses/testFile.php
@@ -1,13 +1,18 @@
 <?php
-
 use PHPUnit\Framework\TestCase;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-class CalculatorTest extends TestCase
-{
-    public function testAddition()
-    {
+class CalculatorTest extends TestCase {
+    public function testAddition() {
         $calculator = new Calculator();
         $result = $calculator->add(2, 3);
         $this->assertEquals(5, $result);
+    }
+
+    public function testSubtraction() {
+        $calculator = new Calculator();
+        $result = $calculator->sub(5, 2);
+        $this->assertEquals(3, $result);
     }
 }
