e1390c463a132d70856364dda572d6542f8a31de
FoolyxCooly <82619286+FoolyxCooly@users.noreply.github.com>
2024-11-22 20:37:58 -0500

    yes

---
+++ TestClasses/testFile.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAddition()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+}
